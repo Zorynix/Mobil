90c831f6945ff93842b3bfb1e34a82977ead21af
Eckorezze <alexandrsoloby@gmail.com>
2024-03-10 22:47:15 +0300

    fix2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
 
     implementation("androidx.fragment:fragment:$fragment_version")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("android.support:leanback-v17:28.0.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
